3089510939c81154fee20c1ebaf4e26cd5c0975e
DavidCoenFish <fish_dsc@yahoo.co.uk>
2025-05-10 20:54:29 +1000

    2025-05-10T20:54:24 DESKTOP-LD41L9F

--- code/staticlib/dsc_common/source/angle.cpp
+++ code/staticlib/dsc_common/source/angle.cpp
@@ -1,1 +1,13 @@
 #include "angle.h"
+
+const float Angle::DegToRadian(const float in_deg)
+{
+	return in_deg * DSC_PI_DIV_180;
+}
+
+const float Angle::RadianToDeg(const float in_radian)
+{
+	return in_radian * DSC_180_DIV_PI;
+}
+
+
---
+++ code/staticlib/dsc_common/source/angle.h
@@ -0,0 +1,5 @@
+namespace Angle
+{
+	const float DegToRadian(const float in_deg);
+	const float RadianToDeg(const float in_radian);
+}
