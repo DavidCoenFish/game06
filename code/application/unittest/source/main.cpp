77b527f456a31f72bb5f7f67692f192eccef6461
DavidCoenFish <fish_dsc@yahoo.co.uk>
2025-05-11 16:01:58 +1000

    2025-05-11T16:01:46 DESKTOP-LD41L9F

--- code/application/unittest/source/main.cpp
+++ code/application/unittest/source/main.cpp
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 
-int main(int argc, char* argv[], char* envp[])
+//int main(int argc, char* argv[], char* envp[])
+int main(int, char*, char*)
 {
 	printf("Hello world\n");
 	return 0;
