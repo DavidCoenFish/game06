7f915a87b20fae376cbc4515ff5982a652bc3f0b
DavidC <fish_dsc@yahoo.co.uk>
2025-06-06 22:21:18 +1000

    2025-06-06T22:21:14 David-C

--- code/staticlib/dsc_render_resource/dsc_render_resource/render_target_pool.h
+++ code/staticlib/dsc_render_resource/dsc_render_resource/render_target_pool.h
@@ -1,7 +1,5 @@
 #pragma once
 #include <dsc_common/dsc_common.h>
-#include <dsc_render/i_render_target.h>
-#include <dsc_render/i_resource.h>
 
 namespace DscRender
 {
@@ -13,101 +11,5 @@ namespace DscRender
 
 namespace DscRenderResource
 {
-	class RenderTargetTexture : public DscRender::IRenderTarget, public DscRender::IResource
-	{
-	public:
-		struct Resource
-		{
-		public:
-			explicit Resource(
-				const D3D12_CLEAR_VALUE& in_clear_value,
-				const DXGI_FORMAT in_format = DXGI_FORMAT_UNKNOWN,
-				const Microsoft::WRL::ComPtr<ID3D12Resource>& in_render_target = nullptr,
-				const std::shared_ptr<DscRender::HeapWrapperItem>& in_render_target_view_descriptor = nullptr,
-				const std::shared_ptr<DscRender::HeapWrapperItem>& in_shader_resource_view_descriptor = nullptr,
-				const bool in_clear_color = false,
-				const bool in_clear_depth = false,
-				const bool in_clear_stencil = false
-			);
 
-		public:
-			D3D12_CLEAR_VALUE _clear_value;
-			DXGI_FORMAT _format;
-			Microsoft::WRL::ComPtr < ID3D12Resource > _render_target;
-			std::shared_ptr < DscRender::HeapWrapperItem > _render_target_view_descriptor;
-			std::shared_ptr < DscRender::HeapWrapperItem > _shader_resource_view_descriptor;
-			bool _clear_color;
-			bool _clear_depth;
-			bool _clear_stencil;
-		};
-		RenderTargetTexture(
-			DscRender::DrawSystem* const in_draw_system,
-			const std::vector < DscRender::RenderTargetFormatData >& in_target_format_data_array,
-			const DscRender::RenderTargetDepthData& in_target_depth_data,
-			const int32 in_size_width,
-			const int32 in_size_height,
-			const bool in_resize_with_screen = false
-		);
-		virtual ~RenderTargetTexture();
-		std::shared_ptr<DscRender::HeapWrapperItem> GetShaderResourceHeapWrapperItem(const int in_index = 0) const;
-		std::shared_ptr<DscRender::HeapWrapperItem> GetDepthResourceHeapWrapperItem() const;
-		std::shared_ptr<DscRender::HeapWrapperItem> GetDepthShaderResourceHeapWrapperItem() const;
-		void Resize(
-			ID3D12GraphicsCommandList* const in_command_list,
-			ID3D12Device2* const in_device,
-			const int32 in_size_width,
-			const int32 in_size_height
-		);
-
-	private:
-		virtual void OnDeviceLost() override;
-		virtual void OnDeviceRestored(
-			ID3D12GraphicsCommandList* const in_command_list,
-			ID3D12Device2* const in_device
-		) override;
-		void DeviceRestored(
-			ID3D12Device2* const in_device
-		);
-		virtual void OnResize(
-			ID3D12GraphicsCommandList* const in_command_list,
-			ID3D12Device2* const in_device,
-			const int32 in_size_width,
-			const int32 in_size_height
-		) override;
-		virtual void StartRender(ID3D12GraphicsCommandList* const in_command_list, const bool in_allow_clear) override;
-		virtual void EndRender(ID3D12GraphicsCommandList* const in_command_list) override;
-		void TransitionResource(
-			ID3D12GraphicsCommandList* const in_command_list,
-			const D3D12_RESOURCE_STATES in_new_state_render_target,
-			const D3D12_RESOURCE_STATES in_new_state_depth_resource
-		);
-		// D3D12_RESOURCE_STATE_DEPTH_WRITE	= 0x10,
-		// D3D12_RESOURCE_STATE_DEPTH_READ	= 0x20,
-		// Virtual void GetPipelineState(D3D12_GRAPHICS_PIPELINE_STATE_DESC& psoDesc) const override;
-		virtual void GetFormatData(
-			DXGI_FORMAT& in_depth_format,
-			int& in_render_target_view_format_count,
-			const DXGI_FORMAT*& in_render_target_view_format
-		) const override;
-		virtual const int32 GetWidth() const override;
-		virtual const int32 GetHeight() const override;
-
-	public:
-		std::vector < std::shared_ptr < Resource > > _target_resource_array;
-		std::vector < D3D12_CPU_DESCRIPTOR_HANDLE > _array_render_target_descriptors;
-		// Cache the cpu handle of the target resource
-		std::shared_ptr < Resource > _depth_resource;
-		D3D12_VIEWPORT _screen_viewport;
-		D3D12_RECT _scissor_rect;
-		D3D12_RESOURCE_STATES _current_state_render_target;
-		D3D12_RESOURCE_STATES _current_state_depth_resource;
-
-	private:
-		int32 _size_width;
-		int32 _size_height;
-		bool _resize_with_screen;
-		std::vector < DXGI_FORMAT > _target_format_array;
-		int _id;
-
-	};
 }//namespace DscRenderResource
