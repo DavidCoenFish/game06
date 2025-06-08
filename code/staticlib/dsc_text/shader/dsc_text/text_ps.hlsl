98290a9a6665eaebf434015b5bf760bd4e852c0b
DavidC <fish_dsc@yahoo.co.uk>
2025-06-08 23:36:53 +1000

    2025-06-08T23:36:48 David-C

--- code/staticlib/dsc_text/shader/dsc_text/text_ps.hlsl
+++ code/staticlib/dsc_text/shader/dsc_text/text_ps.hlsl
@@ -1,20 +1,26 @@
 #include "text_interpolant.hlsli"
 
-Texture2D g_texture : register(t0);
-SamplerState g_sampler_state : register(s0);
-
 struct Pixel
 {
     float4 _colour : SV_TARGET0;
 };
+
+Texture2D g_texture : register(t0);
+SamplerState g_sampler_state : register(s0);
 
 Pixel main(Interpolant in_input)
 {
     Pixel result;
 
     float4 texel = g_texture.Sample(g_sampler_state, in_input._uv);
-
-    result._colour = texel;
+    float alpha = dot(texel, in_input._mask);
+    // Premultiplied alpha blending
+    result._colour = float4(
+        in_input._colour.x * alpha,
+        in_input._colour.y * alpha,
+        in_input._colour.z * alpha,
+        in_input._colour.w * alpha
+        );
 
     return result;
 }
--- code/staticlib/dsc_text/shader/dsc_text/text_vs.hlsl
+++ code/staticlib/dsc_text/shader/dsc_text/text_vs.hlsl
@@ -3,9 +3,15 @@
 struct Vertex
 {
     // [-1 ... 1]
-    float2 _position : POSITION;
-    // [0 ... 1]
+    float2 _position : Position;
     float2 _uv : TEXCOORD0;
+
+    // Bandwidth vrs book-keeping to convert choice of rgba channel for data as foat4 or int
+    // Mask also allows us to have icons in the font texture page if we want to be clever
+    float4 _mask : COLOR0;
+
+    float4 _colour : COLOR1;
+
 };
 
 Interpolant main(Vertex in_input)
@@ -14,9 +20,11 @@ Interpolant main(Vertex in_input)
     result._position = float4(
         in_input._position.x,
         in_input._position.y,
-        0.0f, 
+        0.0f,
         1.0f
         );
     result._uv = in_input._uv;
+    result._mask = in_input._mask;
+    result._colour = in_input._colour;
     return result;
 }
